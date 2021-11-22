329708b55675dc0afc02dccdbb40481480d1b09e
Andrew Churchill <chu.andrew.8@outlook.com>
2021-11-22 02:04:57 -0500

    clean up code

--- hooks/useMouseCameraControl.ts
+++ hooks/useMouseCameraControl.ts
@@ -1,4 +1,4 @@
-import { mat4, ReadonlyVec3, vec3, vec4 } from "gl-matrix";
+import { mat4, vec4 } from "gl-matrix";
 import React, { useCallback, useRef } from "react";
 import { CAMERA_DISTANCE } from "utils/constants";
 
@@ -9,17 +9,17 @@ export default function useMouseCameraControl(
   viewMatrix: React.MutableRefObject<mat4>,
   render: () => void
 ): Record<MouseEventName, (e: React.MouseEvent) => void> {
-  const lastMousePos = useRef<{ x: number; y: number; viewMatrix: mat4 }>({
+  const lastMousePos = useRef<{ x: number; y: number; cameraMatrix: mat4 }>({
     x: 0,
     y: 0,
-    viewMatrix: viewMatrix.current,
+    cameraMatrix: viewMatrix.current,
   });
   const onMouseDown = useCallback(
     (e: React.MouseEvent) => {
       lastMousePos.current = {
         x: e.clientX,
         y: e.clientY,
-        viewMatrix: mat4.invert(mat4.create(), viewMatrix.current),
+        cameraMatrix: mat4.invert(mat4.create(), viewMatrix.current),
       };
     },
     [viewMatrix]
@@ -36,22 +36,23 @@ export default function useMouseCameraControl(
 
       const rotation = mat4.getRotation(
         vec4.create(),
-        lastMousePos.current.viewMatrix
+        lastMousePos.current.cameraMatrix
       );
-      // start with rotation from last time
-      mat4.fromQuat(viewMatrix.current, rotation);
-      mat4.rotateY(viewMatrix.current, viewMatrix.current, theta);
-      mat4.rotateX(viewMatrix.current, viewMatrix.current, phi);
-      mat4.translate(viewMatrix.current, viewMatrix.current, [
-        0,
-        0,
-        CAMERA_DISTANCE,
-      ]);
-      mat4.invert(viewMatrix.current, viewMatrix.current);
-      // mat4.rotateY(viewMatrix.current, viewMatrix.current, theta);
 
-      render();
-      // requestAnimationFrame(render);
+      // start with rotation from last time
+      const cameraMatrix = mat4.fromQuat(mat4.create(), rotation);
+
+      // apply rotation from mouse movement
+      mat4.rotateY(cameraMatrix, cameraMatrix, theta);
+      mat4.rotateX(cameraMatrix, cameraMatrix, phi);
+
+      // apply offset to move us away from square
+      mat4.translate(cameraMatrix, cameraMatrix, [0, 0, CAMERA_DISTANCE]);
+
+      // set view matrix to inverse of camera matrix
+      mat4.invert(viewMatrix.current, cameraMatrix);
+
+      requestAnimationFrame(render);
     },
     [gl, viewMatrix, render]
   );
