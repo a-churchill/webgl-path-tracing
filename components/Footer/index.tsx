722236447f3b3bda145f371e8f76cfe1fb644a7e
Andrew Churchill <chu.andrew.8@outlook.com>
2021-12-07 23:45:28 -0500

    update footer

--- components/Footer/index.tsx
+++ components/Footer/index.tsx
@@ -19,6 +19,21 @@ export default function Footer() {
           height={24}
         />
       </a>
+      <span className={styles.divider} />
+      Source code{" "}
+      <a
+        href="https://github.com/a-churchill/webgl-path-tracing"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        <Image
+          className={styles.logo}
+          src="/github-logo.png"
+          alt="GitHub logo"
+          width={24}
+          height={24}
+        />
+      </a>
     </footer>
   );
 }
